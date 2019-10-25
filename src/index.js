89f8bcb45499e2fad4c5878a30baac9b57cdee7d
Charles Stevens <charles.stevens88@gmail.com>
2019-10-24 22:48:12 -0400

    Remove material-ui

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-
-import ChainBadge from './components/chainBadge';
-import DateBox from './components/dateBox';
-import HabitChain from './components/habitChain';
-import Header from './components/header';
 
 class App extends React.Component {
     constructor(props) {
@@ -17,39 +9,7 @@ class App extends React.Component {
         }
     }
     render() {
-        let date = new Date();
-        return (
-            <Container class="app-root">
-                    <Grid item>
-                        <Grid item
-                        direction="column"
-                        justify="flex-start"
-                        alignItems="flex-start"
-                        color="white"
-                        >
-                        <Grid item>
-                            <DateBox
-                                fullDate={new Date()}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <DateBox
-                                fullDate={new Date(date.setDate(date.getDate() - 1))}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <DateBox
-                                fullDate={new Date(date.setDate(date.getDate() - 1))}
-                            />
-                        </Grid>
-                    </Grid>
-                    </Grid>
-                    <Grid item>
-                        <HabitChain name="habit1"/>
-                    </Grid>
-                <Header/>
-            </Container>
-        );
+        return ();
     }
 }
 
